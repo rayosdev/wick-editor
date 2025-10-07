db18789e662aa73cb4258521d4f4b65b805afccf
rayosdev <rayos-dev@gmail.com>
2025-10-07 23:34:22 +0200

    feat: Update WICK_ENGINE_BUILD_VERSION to 2025.10.7.23.18.51

--- engine/dist/wickengine.js
+++ engine/dist/wickengine.js
@@ -48,7 +48,7 @@ if (typeof __dirname === "undefined") { var __dirname = ""; }
 if (typeof __filename === "undefined") { var __filename = ""; }
 
 /*Wick Engine https://github.com/Wicklets/wick-engine*/
-var WICK_ENGINE_BUILD_VERSION = "2025.10.7.21.26.8";
+var WICK_ENGINE_BUILD_VERSION = "2025.10.7.23.18.51";
 
 /*!
  * Paper.js v0.12.4 - The Swiss Army Knife of Vector Graphics Scripting.
